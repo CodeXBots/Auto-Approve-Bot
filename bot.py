ae6bd668d8e6532c37d3c6ae4ac96bc28317274a
Code X Bots <125588667+codexbots@users.noreply.github.com>
2024-05-26 23:54:02 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -53,7 +53,7 @@ async def op(_, m :Message):
                 ]
             )
             add_user(m.from_user.id)
-            await m.reply_photo("https://telegra.ph/file/63d723680cca52ba46319.jpg", caption="**{}\nI'm an auto approve [Admin Join Requests]({}) Bot.\nI can approve users in Groups/Channels.Add me to your Channel Or Group and promote me Admin with add members permission.\n\n__By : @RahulReviewsYT__**".format(m.from_user.mention, "https://telegram.me/RahulReviewsYT"), reply_markup=keyboard)
+            await m.reply_photo("https://graph.org/file/7ff4c48c2e42aecfbcffa.jpg", caption="**{}\nI'm an auto approve [Admin Join Requests]({}) Bot.\nI can approve users in Groups/Channels.Add me to your Channel Or Group and promote me Admin with add members permission.\n\n__By : @RahulReviewsYT__**".format(m.from_user.mention, "https://telegram.me/RahulReviewsYT"), reply_markup=keyboard)
     
         elif m.chat.type == enums.ChatType.GROUP or enums.ChatType.SUPERGROUP:
             keyboar = InlineKeyboardMarkup(
