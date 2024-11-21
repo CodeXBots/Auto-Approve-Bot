475c749b3f7f0c26dfbe3570a10c38ccda61a96e
CodeXBots <RahulReviewsYT@gmail.com>
2024-11-21 19:23:59 +0530

    Add files via upload

---
+++ CodeXBots/admin.py
@@ -0,0 +1,98 @@
+from pyrogram.types import Message
+from pyrogram import Client, filters
+from pyrogram.errors import FloodWait, InputUserDeactivated, UserIsBlocked, PeerIdInvalid
+import os, sys, time, asyncio, logging, datetime, pytz
+from CodeXBots.database import rkn_botz
+from configs import rkn1
+
+
+@Client.on_message(filters.command(["stats", "status"]) & filters.user(rkn1.ADMIN))
+async def get_stats(bot, message):
+    total_users = await rkn_botz.total_users_count()
+    uptime = time.strftime("%Hh%Mm%Ss", time.gmtime(time.time() - bot.uptime))    
+    start_t = time.time()
+    rkn = await message.reply('**ᴘʀᴏᴄᴇssɪɴɢ.....**')    
+    end_t = time.time()
+    time_taken_s = (end_t - start_t) * 1000
+    await rkn.edit(text=f"**--Bᴏᴛ Sᴛᴀᴛᴜꜱ--** \n\n**⌚️ Bᴏᴛ Uᴩᴛɪᴍᴇ:** {uptime} \n**🐌 Cᴜʀʀᴇɴᴛ Pɪɴɢ:** `{time_taken_s:.3f} ᴍꜱ` \n**👭 Tᴏᴛᴀʟ Uꜱᴇʀꜱ:** `{total_users}`")
+
+# Restart to cancell all process 
+@Client.on_message(filters.private & filters.command("restart") & filters.user(rkn1.ADMIN))
+async def restart_bot(b, m):
+    rkn = await b.send_message(text="**🔄 ᴘʀᴏᴄᴇssᴇs sᴛᴏᴘᴘᴇᴅ. ʙᴏᴛ ɪs ʀᴇsᴛᴀʀᴛɪɴɢ.....**", chat_id=m.chat.id)
+    failed = 0
+    success = 0
+    deactivated = 0
+    blocked = 0
+    start_time = time.time()
+    total_users = await rkn_botz.total_users_count()
+    all_users = await rkn_botz.get_all_users()
+    async for user in all_users:
+        try:
+            restart_msg = f"ʜᴇʏ, {(await b.get_users(user['_id'])).mention}\n\n**🔄 ᴘʀᴏᴄᴇssᴇs sᴛᴏᴘᴘᴇᴅ. ʙᴏᴛ ɪs ʀᴇsᴛᴀʀᴛɪɴɢ.....\n\n✅️ ʙᴏᴛ ɪs ʀᴇsᴛᴀʀᴛᴇᴅ. ɴᴏᴡ ʏᴏᴜ ᴄᴀɴ ᴜsᴇ ᴍᴇ.**"
+            await b.send_message(user['_id'], restart_msg)
+            success += 1
+        except InputUserDeactivated:
+            deactivated +=1
+            await rkn_botz.delete_user(user['_id'])
+        except UserIsBlocked:
+            blocked +=1
+            await rkn_botz.delete_user(user['_id'])
+        except Exception as e:
+            failed += 1
+            await rkn_botz.delete_user(user['_id'])
+            print(e)
+            pass
+        try:
+            await rkn.edit(f"<u>ʀᴇsᴛᴀʀᴛ ɪɴ ᴩʀᴏɢʀᴇꜱꜱ:</u>\n\n• ᴛᴏᴛᴀʟ ᴜsᴇʀs: {total_users}\n• sᴜᴄᴄᴇssғᴜʟ: {success}\n• ʙʟᴏᴄᴋᴇᴅ ᴜsᴇʀs: {blocked}\n• ᴅᴇʟᴇᴛᴇᴅ ᴀᴄᴄᴏᴜɴᴛs: {deactivated}\n• ᴜɴsᴜᴄᴄᴇssғᴜʟ: {failed}")
+        except FloodWait as e:
+            await asyncio.sleep(e.value)
+    completed_restart = datetime.timedelta(seconds=int(time.time() - start_time))
+    await rkn.edit(f"ᴄᴏᴍᴘʟᴇᴛᴇᴅ ʀᴇsᴛᴀʀᴛ: {completed_restart}\n\n• ᴛᴏᴛᴀʟ ᴜsᴇʀs: {total_users}\n• sᴜᴄᴄᴇssғᴜʟ: {success}\n• ʙʟᴏᴄᴋᴇᴅ ᴜsᴇʀs: {blocked}\n• ᴅᴇʟᴇᴛᴇᴅ ᴀᴄᴄᴏᴜɴᴛs: {deactivated}\n• ᴜɴsᴜᴄᴄᴇssғᴜʟ: {failed}")
+    os.execl(sys.executable, sys.executable, *sys.argv)
+    
+    
+@Client.on_message(filters.command("broadcast") & filters.user(rkn1.ADMIN) & filters.reply)
+async def broadcast_handler(bot: Client, m: Message):
+    await bot.send_message(rkn1.LOG_CHANNEL, f"{m.from_user.mention} or {m.from_user.id} Iꜱ ꜱᴛᴀʀᴛᴇᴅ ᴛʜᴇ Bʀᴏᴀᴅᴄᴀꜱᴛ......")
+    all_users = await rkn_botz.get_all_users()
+    broadcast_msg = m.reply_to_message
+    sts_msg = await m.reply_text("Bʀᴏᴀᴅᴄᴀꜱᴛ Sᴛᴀʀᴛᴇᴅ..!") 
+    done = 0
+    failed = 0
+    success = 0
+    start_time = time.time()
+    total_users = await rkn_botz.total_users_count()
+    async for user in all_users:
+        sts = await send_msg(user['_id'], broadcast_msg)
+        if sts == 200:
+           success += 1
+        else:
+           failed += 1
+        if sts == 400:
+           await rkn_botz.delete_user(user['_id'])
+        done += 1
+        if not done % 20:
+           await sts_msg.edit(f"Bʀᴏᴀᴅᴄᴀꜱᴛ Iɴ Pʀᴏɢʀᴇꜱꜱ: \nTᴏᴛᴀʟ Uꜱᴇʀꜱ {total_users} \nCᴏᴍᴩʟᴇᴛᴇᴅ: {done} / {total_users}\nSᴜᴄᴄᴇꜱꜱ: {success}\nFᴀɪʟᴇᴅ: {failed}")
+    completed_in = datetime.timedelta(seconds=int(time.time() - start_time))
+    await sts_msg.edit(f"Bʀᴏᴀᴅᴄᴀꜱᴛ Cᴏᴍᴩʟᴇᴛᴇᴅ: \nCᴏᴍᴩʟᴇᴛᴇᴅ Iɴ `{completed_in}`.\n\nTᴏᴛᴀʟ Uꜱᴇʀꜱ {total_users}\nCᴏᴍᴩʟᴇᴛᴇᴅ: {done} / {total_users}\nSᴜᴄᴄᴇꜱꜱ: {success}\nFᴀɪʟᴇᴅ: {failed}")
+           
+async def send_msg(user_id, message):
+    try:
+        await message.copy(chat_id=int(user_id))
+        return 200
+    except FloodWait as e:
+        await asyncio.sleep(e.value)
+        return send_msg(user_id, message)
+    except InputUserDeactivated:
+        logger.info(f"{user_id} : Dᴇᴀᴄᴛɪᴠᴀᴛᴇᴅ")
+        return 400
+    except UserIsBlocked:
+        logger.info(f"{user_id} : Bʟᴏᴄᴋᴇᴅ Tʜᴇ Bᴏᴛ")
+        return 400
+    except PeerIdInvalid:
+        logger.info(f"{user_id} : Uꜱᴇʀ Iᴅ Iɴᴠᴀʟɪᴅ")
+        return 400
+    except Exception as e:
+        logger.error(f"{user_id} : {e}")
+        return 500
---
+++ CodeXBots/database.py
@@ -0,0 +1,42 @@
+import motor.motor_asyncio, datetime, pytz
+from configs import rkn1
+
+class Database:
+    def __init__(self, uri, database_name):
+        self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
+        self.db = self._client[database_name]
+        self.col = self.db.user
+        
+    def new_user(self, id):
+        return dict(_id=int(id))
+   
+    async def add_user(self, b, m):
+        u = m.from_user
+        if not await self.is_user_exist(u.id):
+            user = self.new_user(u.id)
+            await self.col.insert_one(user)            
+            await self.send_user_log(b, u)
+
+    async def is_user_exist(self, id):
+        user = await self.col.find_one({'_id': int(id)})
+        return bool(user)
+
+    async def total_users_count(self):
+        count = await self.col.count_documents({})
+        return count
+
+    async def get_all_users(self):
+        all_users = self.col.find({})
+        return all_users
+
+    async def delete_user(self, user_id):
+        await self.col.delete_many({'_id': int(user_id)})
+    
+    async def send_user_log(self, b, u):
+        if rkn1.LOG_CHANNEL is not None:
+            await b.send_message(
+            rkn1.LOG_CHANNEL,
+            f"**--Nᴇᴡ Uꜱᴇʀ Sᴛᴀʀᴛᴇᴅ Tʜᴇ Bᴏᴛ--**\n\nUꜱᴇʀ: {u.mention}\nIᴅ: `{u.id}`\nUɴ: @{u.username}\n\nBy: {b.mention}"
+        )
+        
+rkn_botz = Database(rkn1.DB_URL, rkn1.DB_NAME)
