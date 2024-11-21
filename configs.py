51f048f86d153a4da86c00eec6413816a321be3c
CodeXBots <RahulReviewsYT@gmail.com>
2024-11-21 19:19:01 +0530

    Create configs.py

---
+++ configs.py
@@ -0,0 +1,21 @@
+from os import path, getenv
+import os, time 
+
+class Config:
+    API_ID = int(getenv("API_ID", ""))
+    API_HASH = getenv("API_HASH", "")
+    BOT_TOKEN = getenv("BOT_TOKEN", "")
+ 
+    ADMIN = list(map(int, getenv("ADMIN", "").split()))
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+    
+    # database configs
+    DB_URL = os.environ.get("DB_URL", "")
+    DB_NAME = os.environ.get("DB_NAME", "")
+    
+    #web response 
+    WEBHOOK = bool(os.environ.get("WEBHOOK", True))
+    BOT_UPTIME  = time.time()
+    PORT = os.environ.get("PORT", "8080")
+
+rkn1 = Config()
